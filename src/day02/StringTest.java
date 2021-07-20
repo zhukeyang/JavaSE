3c0c3516328fd536993c22af523419c426b76a09
zhu <zhu@qq.com>
2021-07-20 17:55:48 +0800

    third commit

---
+++ src/day02/StringTest.java
@@ -0,0 +1,26 @@
+package day02;
+
+import org.junit.Test;
+
+/*
+* String的使用
+*
+*
+* */
+public class StringTest {
+
+//    string字符串，使用一对""引起来表示
+    /*
+    * String声明为final，不可被继承
+    * 2.String实现了Serializable接口，表示字符串是支持序列化的
+    *         实现了Comparable接口，表示String可以比较大小
+    * 3.String内部定义了final char[] value用于存储字符串数据，数组的元素也不能在修改了
+    * 4.String：代表不可变的字符序列
+    * */
+
+    @Test
+    public void test1()
+    {
+
+    }
+}
